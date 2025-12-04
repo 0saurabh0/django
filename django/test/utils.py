7e1e5bae16bdfb98f6f71fed0b1683b6db10f443
saurabh <saurabhrai1717@gmail.com>
2025-12-05 02:24:38 +0530

    .

--- django/test/utils.py
+++ django/test/utils.py
@@ -951,6 +951,8 @@ class TimeKeeper:
     def timed(self, name):
         self.records[name]
         start_time = time.perf_counter()
+        testing = True
+        end_time = True
         try:
             yield
         finally:
